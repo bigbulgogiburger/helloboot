bbc41e790f5803b0e503ad6019011167fdd3e70d
pyeondohun <dohoon321@gmail.com>
2024-11-09 17:37:16 +0900

    EnableMyConfigurationProperties를 사용해서 자동 구성 함.

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,27 +1,33 @@
 package tobyspring.config.autoconfig;
 
-import com.fasterxml.jackson.databind.util.ClassUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
 
 // 원래 configuration이였는데 관례상 @MyAutoConfiguration넣음
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
+
 
     @Bean("tomcatWebServerFactory")
     //해당 bean이 유저가 커스텀으로 만든게 등록되어 있으면 등록 안함.
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment environment) {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(environment.getProperty("contextPath"));
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
         return factory;
     }
+
+
 }
